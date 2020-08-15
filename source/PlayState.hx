f9c8bcddf091799aa198dbc892ac523627689ed9
Stan Hughes <hughes.stan@gmail.com>
2020-08-14 21:16:24 -0400

    Working Build

---
+++ source/PlayState.hx
@@ -0,0 +1,89 @@
+package;
+
+import flixel.FlxState;
+import flixel.text.FlxText;
+import flixel.ui.FlxButton;
+import flixel.util.FlxSave;
+import flixel.util.FlxStringUtil;
+
+class PlayState extends FlxState
+{
+	final chapters = 239;
+	var chapterCount = 0;
+	var chapterRead:FlxText;
+	var percentageRead:FlxText;
+	var percentageToRead:FlxText;
+	var saveState:FlxSave;
+
+	override public function create()
+	{
+		super.create();
+
+		final spacing = 50;
+		final rightMargin = 70;
+		final titleSize = 24;
+		final labelSize = 16;
+
+		saveState = new FlxSave();
+		saveState.bind('SaveState');
+
+		if (saveState.data.chapterCount == null)
+		{
+			trace('creating initial chapter count');
+			saveState.data.chapterCount = 0;
+		}
+		else
+		{
+			trace('loading saved data');
+			chapterCount = saveState.data.chapterCount;
+		}
+
+		add(new flixel.text.FlxText(rightMargin, 1 * spacing, 0, 'Chapter Master', titleSize));
+
+		chapterRead = new FlxText(rightMargin, 2 * spacing, 0, 'Chapters Read: ${chapterCount}', labelSize);
+		add(chapterRead);
+
+		percentageRead = new FlxText(rightMargin, 3 * spacing, 0, 'Percentage Read: ${(chapterCount / chapters) * 100} percent', labelSize);
+		add(percentageRead);
+
+		percentageToRead = new FlxText(rightMargin, 4 * spacing, 0, 'Percentage to Read: ${100 - ((chapterCount / chapters) * 100)} percent', labelSize);
+		add(percentageToRead);
+
+		var currentLocation = new FlxText(rightMargin, 5 * spacing, 0, 'Current Location: 1 Nephi 1:1', labelSize);
+		add(currentLocation);
+
+		var readChapter = new FlxButton(2 * rightMargin, 6 * spacing, 'Read Chapter', readChapterPushed);
+		add(readChapter);
+	}
+
+	function readChapterPushed()
+	{
+		chapterCount += 1;
+		saveTheState();
+	}
+
+	override public function update(elapsed:Float)
+	{
+		chapterRead.text = 'Chapters Read: ${chapterCount}';
+		percentageRead.text = 'Percentage Read: ${FlxStringUtil.formatMoney((chapterCount / chapters) * 100)} percent';
+		percentageToRead.text = 'Percentage to Read: ${FlxStringUtil.formatMoney(100 - ((chapterCount / chapters) * 100))} percent';
+
+		// saveTheState();
+
+		super.update(elapsed);
+	}
+
+	function saveTheState()
+	{
+		saveState.data.chapterCount = chapterCount;
+		saveState.flush();
+		trace('save state');
+	}
+
+	override public function kill()
+	{
+		saveTheState();
+
+		super.kill();
+	}
+}
